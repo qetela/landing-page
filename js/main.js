b9c93be1d6d8ca43df0fe6340fc8a41a4f606373
Kelvin Macharia <ngunyimacharia@gmail.com>
2019-02-12 21:14:19 +0000

    HTML / CSS Changes

--- js/main.js
+++ js/main.js
@@ -16,21 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   })();
 
-  /* Smooth scroll */
-  // Scroll to specific values
-  // scrollTo is the same
-  window.scroll({
-    top: 2500,
-    left: 0,
-    behavior: 'smooth'
-  });
-
-  // Scroll certain amounts from current position
-  window.scrollBy({
-    top: 100, // could be negative value
-    left: 0,
-    behavior: 'smooth'
-  });
 
   /* Modal code */
 
