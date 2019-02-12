6b71f9696b7888ac1a32b1113013ce1490832efe
Kelvin Macharia <ngunyimacharia@gmail.com>
2019-02-12 02:00:04 +0000

    First commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// The following code is based off a toggle menu by @Bradcomp
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+    menu.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: 'smooth'
+});
